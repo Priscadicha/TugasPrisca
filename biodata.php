9d6d33b498cd95ecfeffa30b556041b002f40b1a
Priscadicha <114549151+Priscadicha@users.noreply.github.com>
2022-09-28 11:08:44 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+    <head>
+<title> Biodata</title>
+</head>
+<body>
+<table border = "3" style = background-color:white>
+<tr>
+<td style = background-color:pink colspan = "3"><center><b><font color = white size = 30 >Biodata</font></b></center>  </td>
+</tr>
+<tr>
+<td style = background-color:pink colspan = "3"><center><b><font color = white size = 30 > Universitas Muhammadiyah Purworejo
+</font></b></center></td>
+</tr>
+<tr>
+<td rowspan =25><center><img src = "foto.webp" width = 300 high = 400/></center></td>
+</tr>
+<tr>
+<td>Nim</td>
+<td>202520004</td>
+</tr>
+<tr>
+<td>Nama</td>
+<td>Prisca Dicha K.P</td>
+</tr>
+ <tr>
+<td>Alamat</td>
+<td>Pangenrejo </td>
+</tr>
+ <tr>
+<td>Alamat Email</td>
+<td>priscadicha@gmail.com</td>
+</tr>
+<tr>
+    <td>Bidang Minat</td>
+    <td>kuliner</td>
+</tr>
+  </table>
+  </body>
+ </html>
